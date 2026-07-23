2d156985920fcce08115cd8d9d4634edb533e5e2
GH cyber <89rx77@gmail.com>
2026-07-23 17:54:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import hashlib
+path = "C:/Users/ghost/Downloads/test.txt"
+#destinatia/path -- loactia fisierului
+
+with open(path, "rb") as file:
+	continut = file.read()
+
+file_hash = hashlib.sha256(continut).hexdigest()
+if file_hash == "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855":
+	print("Fisier gol")
+
+else:
+	print(file_hash)
